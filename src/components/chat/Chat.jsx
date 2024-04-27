565e31ec85ac071bdc192157eaf14615fd34e948
Allagba <109833636+Allagba@users.noreply.github.com>
2024-04-26 20:26:29 -0400

    autre

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -24,7 +24,46 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem, ipsum dolor sit amet</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem, ipsum dolor sit amet</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem, ipsum dolor sit amet</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem, ipsum dolor sit amet</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <div className="texts">
+            <p>Lorem, ipsum dolor sit amet</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem, ipsum dolor sit amet</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
